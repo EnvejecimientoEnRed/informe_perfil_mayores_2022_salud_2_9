e6288b607009ae96d5dc2f25f5ad8506ac5603e0
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-26 10:18:59 +0200

    Desarrollo captura PDF con cambio datos

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -9,49 +9,26 @@ import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
 const COLOR_PRIMARY_1 = '#F8B05C',
-COLOR_GREY_1 = '#D6D6D6';
+COLOR_ANAG_PRIM_3 = '#9E3515';
 let tooltip = d3.select('#tooltip');
 
 export function initChart(iframe) {
     ///Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_salud_2_9/main/data/tasas_mortalidad_causa_2001_2020_v2.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_salud_2_9/main/data/tasas_mortalidad_pdf.csv', function(error,data) {
         if (error) throw error;
 
-        //////SELECTOR
-        let tipoCausa = 'I. Infecciosas*';
-        document.getElementById('enfermedad_cronica').addEventListener('change', function(e) {
-            tipoCausa = e.target.value;
-            updateChart(tipoCausa);
+        data.sort(function(x, y){
+            return d3.ascending(+x[2001], +y[2001]);
         });
 
-        let tipoY = 1600;
-        document.getElementById('change_yaxis').addEventListener('click', function(e) {
-            if (tipoY == 1600) {
-                tipoY = 250;
-
-                //Modificamos el texto del botón
-                e.target.textContent = 'Eje Y en tasa 1.600';
-            } else {
-                tipoY = 1600;
-
-                //Modificamos el texto del botón
-                e.target.textContent = 'Eje Y en tasa 250';
-            }
-
-            //Cambiamos eje
-            updateAxis(tipoY);
-        });
-
-        //////VISUALIZACIÓN
-        let paths; 
-        let margin = {top: 10, right: 10, bottom: 25, left: 45},
+        //////VISUALIZACIÓN 
+        let margin = {top: 10, right: 10, bottom: 25, left: 105},
             width = document.getElementById('chart').clientWidth - margin.left - margin.right,
             height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
 
-        let sumstat = d3.nest()
-            .key(function(d) { return d.Causa;})
-            .entries(data);
-
+        let enfermedades = d3.map(data, function(d){return(d.Causa)}).keys().reverse();
+        let tipos = ['2001', '2020'];        
+        
         let svg = d3.select("#chart")
             .append("svg")
                 .attr("width", width + margin.left + margin.right)
@@ -61,28 +38,13 @@ export function initChart(iframe) {
                     "translate(" + margin.left + "," + margin.top + ")");
 
         // Add X axis
-        let x = d3.scaleBand()
-            .domain(d3.map(data, function(d) { return d.Periodo; }).keys())
-            .range([ 0, width ]);
+        let x = d3.scaleLinear()
+            .domain([0,1500])
+            .range([0,width]);
 
         let xAxis = function(g) {
-            g.call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){ if(i == 0 || i == 4 || i == 9 || i == 14 || i == 19){ return d; } })));
-            g.call(function(g){g.selectAll('.tick line').remove()});
-            g.call(function(g){g.select('.domain').remove()});
-        }
-
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        // Add Y axis
-        let y = d3.scaleLinear()
-            .domain([0, 1600])
-            .range([ height, 0 ]);
-
-        let yAxis = function(svg) {
-            svg.call(d3.axisLeft(y).ticks(5).tickFormat(function(d,i) { return numberWithCommas3(d); }));
-            svg.call(function(g) {
+            g.call(d3.axisBottom(x).ticks(5).tickFormat(function(d,i) { return numberWithCommas3(d); }));
+            g.call(function(g) {
                 g.call(function(g){
                     g.selectAll('.tick line')
                         .attr('class', function(d,i) {
@@ -90,192 +52,61 @@ export function initChart(iframe) {
                                 return 'line-special';
                             }
                         })
-                        .attr('x1', '0%')
-                        .attr('x2', `${width}`)
+                        .attr('y1', '0')
+                        .attr('y2', `-${height}`)
                 });
             });
         }
 
         svg.append("g")
-            .attr("class", "yaxis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add Y axis
+        let y = d3.scaleBand()
+            .domain(enfermedades)
+            .range([height,0])
+            .padding(0.35);
+
+        let yAxis = function(g) {
+            g.call(d3.axisLeft(y));
+            g.call(function(g){g.selectAll('.tick line').remove()});
+            g.call(function(g){g.select('.domain').remove()});
+        }
+        
+        svg.append("g")
             .call(yAxis);
 
-        function init(tipoCausa) {
-            //Líneas
-            svg.selectAll(".line")
-                .data(sumstat)
-                .enter()
-                .append("path")
-                .attr('class', 'rect')
-                .attr("fill", "none")
-                .attr("stroke", function(d){ 
-                    if(d.key == tipoCausa) {
-                        return COLOR_PRIMARY_1;
-                    } else {
-                        return COLOR_GREY_1;
-                    }
-                 })
-                .attr("opacity", function(d) {
-                    if(d.key == tipoCausa) {
-                        return '1';
-                    } else {
-                        return '0.5';
-                    }
-                })
-                .attr("stroke-width", function(d) {
-                    if(d.key == tipoCausa) {
-                        return '3';
-                    } else {
-                        return '2';
-                    }
-                })
-                .attr("d", function(d){
-                    return d3.line()
-                        .x(function(d) { return x(d.Periodo) + x.bandwidth() / 2; })
-                        .y(function(d) { return y(+d.Valor); })
-                        (d.values)
-                });
+        let ySubgroup = d3.scaleBand()
+            .domain(tipos)
+            .range([0, y.bandwidth()]);
 
-            //Líneas para animación
-            paths = svg.selectAll('.rect');
-    
-            paths.attr("stroke-dasharray", 1000 + " " + 1000)
-                .attr("stroke-dashoffset", 1000)
-                .transition()
-                .ease(d3.easeLinear)
-                .attr("stroke-dashoffset", 0)
-                .duration(2000);
+        let color = d3.scaleOrdinal()
+            .domain(tipos)
+            .range([COLOR_PRIMARY_1, COLOR_ANAG_PRIM_3]);
 
-            //Círculos para tooltip
-            svg.selectAll('circles')
+        function init() {
+            svg.append("g")
+                .selectAll("g")
                 .data(data)
                 .enter()
-                .append('circle')
-                .attr('class', function(d) {
-                    return 'circle ' + d.Periodo;
-                })
-                .attr('cx', function(d) {
-                    return x(d.Periodo) + x.bandwidth() / 2;
-                })
-                .attr('cy', function(d) {
-                    return y(+d.Valor);
-                })
-                .attr('r', 3)
-                .attr('stroke', 'none')
-                .attr('fill', 'transparent')
-                .on('mouseover', function(d,i,e) {
-                    //Opacidad en círculos
-                    let css = e[i].getAttribute('class').split(' ')[1];
-                    let circles = svg.selectAll('.circle');                    
-            
-                    circles.each(function() {
-                        //this.style.stroke = '0.4';
-                        let split = this.getAttribute('class').split(" ")[1];
-                        if(split == `${css}`) {
-                            this.style.stroke = 'black';
-                            this.style.strokeWidth = '1';
-                        }
-                    });
-
-                    //Texto
-                    let html = '<p class="chart__tooltip--title">' + d.Causa + '</p>' + 
-                        '<p class="chart__tooltip--text">En <b>' + d.Periodo + '</b>, la tasa de mortalidad para este tipo de causa se sitúa en <b>' + numberWithCommas3(parseFloat(d.Valor).toFixed(1)) + '</b> por cada 100.000 habitantes</p>';
-                
-                    tooltip.html(html);
-
-                    //Tooltip
-                    positionTooltip(window.event, tooltip);
-                    getInTooltip(tooltip);
-                })
-                .on('mouseout', function(d,i,e) {
-                    //Quitamos los estilos de la línea
-                    let circles = svg.selectAll('.circle');
-                    circles.each(function() {
-                        this.style.stroke = 'none';
-                    });
-                
-                    //Quitamos el tooltip
-                    getOutTooltip(tooltip); 
-                });
-        }
-
-        function updateChart(tipoCausa) {
-            //Líneas
-            svg.selectAll(".rect")
-                .attr("fill", "none")
-                .attr("stroke", function(d){ 
-                    if(d.key == tipoCausa) {
-                        return COLOR_PRIMARY_1;
-                    } else {
-                        return COLOR_GREY_1;
-                    }
-                 })
-                .attr("opacity", function(d) {
-                    if(d.key == tipoCausa) {
-                        return '1';
-                    } else {
-                        return '0.5';
-                    }
-                })
-                .attr("stroke-width", function(d) {
-                    if(d.key == tipoCausa) {
-                        return '2';
-                    } else {
-                        return '1.5';
-                    }
-                })
-                .transition()
-                .duration(2000)
-                .attr("d", function(d){
-                    return d3.line()
-                        .x(function(d) { return x(d.Periodo) + x.bandwidth() / 2; })
-                        .y(function(d) { return y(+d.Valor); })
-                        (d.values)
-                });
-
-            //Círculos
-            svg.selectAll('.circle')
-                .attr('cx', function(d) {
-                    return x(d.Periodo) + x.bandwidth() / 2;
-                })
-                .attr('cy', function(d) {
-                    return y(+d.Valor);
-                })
-                .attr('r', 3)
-                .attr('stroke', 'none')
-                .attr('fill', 'transparent');
-        }
-
-        function updateAxis(ejeY) {
-            //Modificamos el eje Y de la visualización
-            y.domain([0,ejeY]);
-
-            svg.select(".yaxis")
-                .transition()
-                .duration(2000)
-                .call(yAxis);
-
-            updateChart(tipoCausa);
-        }
-
-        function animateChart() {
-            paths.attr("stroke-dasharray", 1000 + " " + 1000)
-                .attr("stroke-dashoffset", 1000)
-                .transition()
-                .ease(d3.easeLinear)
-                .attr("stroke-dashoffset", 0)
-                .duration(2000);
+                .append("g")
+                .attr("transform", function(d) { return "translate(0," + y(d.Causa) + ")"; })
+                .selectAll("rect")
+                .data(function(d) { return tipos.map(function(key) { return {key: key, value: d[key]}; }); })
+                .enter()
+                .append("rect")
+                .attr('y', function(d) { console.log(d); return ySubgroup(d.key); })
+                .attr('height', ySubgroup.bandwidth())
+                .attr("fill", function(d) { return color(d.key); })
+                .attr('x', function(d) { return x(0); })
+                .attr('width', function(d) { return x(d.value) -x(0); });
         }
 
         //////
         ///// Resto - Chart
         //////
-        init(tipoCausa);
-
-        //Animación del gráfico
-        document.getElementById('replay').addEventListener('click', function() {
-            animateChart();
-        });
+        init();
 
         //////
         ///// Resto
